e7184aafb5de1bd8bde4e951c78cf3367c97ca58
Durgam-vamshi <136604580+Durgam-vamshi@users.noreply.github.com>
2025-03-18 12:15:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ const connectDB = async () => {
             serverSelectionTimeoutMS: 5000, // Optional: Set a timeout
         });
 
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
         return conn;
     } catch (error) {
-        console.error(`❌ MongoDB Connection Failed: ${error.message}`);
+        console.error(`MongoDB Connection Failed: ${error.message}`);
         process.exit(1);
     }
 };
