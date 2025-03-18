976b25636d9bd2d428ca05346051a1d0e22e7339
Durgam-vamshi <136604580+Durgam-vamshi@users.noreply.github.com>
2025-03-18 12:09:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.json());
         app.use("/api", userRoutes);
 
         const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     } catch (error) {
-        console.error("❌ Server failed to start:", error.message);
+        console.error("Server failed to start:", error.message);
         process.exit(1);
     }
 })();
